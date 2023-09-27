e62ff700e9aa3f019f484d6ca7a8c7f4d34a16d0
Nitish Pandey <pandeynitish3438@gmail.com>
2023-09-27 19:49:50 +0530

    changed url in index.js in server

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
     cors(
         {
-            origin: "http://localhost:3000",
+            origin: "https://study-notion-an-ed-tech-mern-stack-project-frontend.vercel.app",
             credentials: true
         }
     )
